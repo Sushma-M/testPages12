ffeb3bccd6714b679b91d54e20d2ea3656074d52
Sushma Wavemaker <sushma.maddirala@wavemaker.com>
2017-12-07 10:29:55 +0000

    test3434 New files added - [Main_Copy_Copy2,NEwduplicatepage,testnewpage,AdventureWorks2014,CountryInfoService,FeedService,FileService,MyJavaService,googleapis,heartRate,hrdb,en.json] Files modified - [Common,Login,Main,footer,header,leftnav,rightnav,topnav,securityService,WEB-INF,data,fonts,lib,logos,material,momentLocale,ngLocale,pages,profiles,resources,webapp,.gitignore,.wmproject.properties,README.md,pom.xml,README.md,Readme.txt,Readme.txt,README.md]

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "testPages12",
+  "homePage" : "Main",
+  "name" : "testPages12",
+  "platformType" : "WEB",
+  "securityEnabled" : "true",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
